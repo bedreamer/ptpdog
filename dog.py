0d21aa34366246a49c0e2c328b116d8a12130d0f
tom <bedreamer@163.com>
2016-05-11 09:36:21 +0800

    Update dog.py

--- dog.py
+++ dog.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'lijie'
 from Tkinter import *
+import  threading
 
 class point:
     def __init__(self, root, id, v, name, x, y, unit):
@@ -9,13 +10,22 @@ class point:
         self.name = name
         self.x = x
         self.y = y
+        self.checked = False
         self.unit = unit
         self.s = StringVar(root)
         self.s.set(v + ' ' + unit)
-        self.c = Checkbutton(root, text=name)
+        self.c = Checkbutton(root, text=name, command=self.check_call_back)
         self.e = Entry(root, width="8", textvariable=self.s)
         self.c.grid(row=x, column=y, sticky="w")
         self.e.grid(row=x, column=y+1)
+    def check_call_back(self):
+        if self.checked:
+            print "uncheck", self.id, self.s.get()
+            self.checked = False
+        else:
+            print "check", self.id, self.s.get()
+            self.checked = True
+
     def set_v(self, v):
         self.s.set(v)
 
@@ -25,6 +35,7 @@ class point:
 class UI:
     def __init__(self):
         self.root = Tk()
+        #self.log = Tk()
         self.client = []
         self.server = []
 
@@ -45,43 +56,95 @@ class UI:
                 x = x + 1
                 y = 0
             p.append(pt)
-        return y + 1
+        return x + 1
 
     def load_client_point(self, f, row):
         row = self._load_point(self.client, f, row)
         self.client_refresh_ttl = StringVar(self.root)
+        print row
         row = row + 1
-        b = Button(self.root, text="取消选择")
+        b = Button(self.root, text="取消选择", command=self.uncheck_all_client)
         b.grid(row=row, column=0)
-        b = Button(self.root, text="全选")
+        b = Button(self.root, text="全选", command=self.check_all_client)
         b.grid(row=row, column=1)
-        b = Button(self.root, text="添加刷新")
+        b = Button(self.root, text="刷新", command=self.refresh_client)
         b.grid(row=row, column=2)
-        return row
+        b = Button(self.root, text="读取", command=self.read_client)
+        b.grid(row=row, column=3)
+        b = Button(self.root, text="写入", command=self.write_client)
+        b.grid(row=row, column=4)
+        return row + 1
 
     def load_server_point(self, f, row):
         row = self._load_point(self.server, f, row)
         self.server_refresh_ttl = StringVar(self.root)
+        print row
         row = row + 1
-        b = Button(self.root, text="取消选择")
+        b = Button(self.root, text="取消选择", command=self.uncheck_all_server)
         b.grid(row=row, column=0)
-        b = Button(self.root, text="全选")
+        b = Button(self.root, text="全选", command=self.check_all_server)
         b.grid(row=row, column=1)
-        b = Button(self.root, text="添加刷新")
+        b = Button(self.root, text="刷新", command=self.refresh_server)
         b.grid(row=row, column=2)
-        return row
+        b = Button(self.root, text="读取", command=self.read_server)
+        b.grid(row=row, column=3)
+        b = Button(self.root, text="写入", command=self.write_server)
+        b.grid(row=row, column=4)
+        return row + 1
+
+    def check_all_client(self):
+        print "check_all_client"
+
+    def uncheck_all_client(self):
+        print "uncheck_all_client"
+
+    def refresh_client(self):
+        print "refresh_client"
+
+    def read_client(self):
+        print "read_client"
+
+    def write_client(self):
+        print "write_client"
+
+    def check_all_server(self):
+        print "check_all_server"
+
+    def uncheck_all_server(self):
+        print "uncheck_all_server"
+
+    def refresh_server(self):
+        print "refresh_server"
+
+    def read_server(self):
+        print "read_server"
+
+    def write_server(self):
+        print "write_server"
+
+    def start_server(self):
+        print u"启动服务器"
+
+    def link_server(self):
+        print u"连接服务器"
 
     def run_main(self):
         self.server_host = StringVar(self.root)
         self.server_addr = StringVar(self.root)
         self.client_addr = StringVar(self.root)
+        self.server_port = StringVar(self.root)
         self.server_host.set("127.0.0.1:9999")
+        self.server_port.set(9999)
+        self.server_addr.set(1)
+        self.client_addr.set(128)
+        self.root.title("CPTP解码狗 V3.45.298")
+        #self.log.title("CPTP数据流")
 
         l = Label(self.root, text=u"服务器地址").grid(row=0, column=0)
         l = Entry(self.root, textvariable=self.server_host, width=30)
         l.grid(row=0, column=1, columnspan=2, sticky="w")
 
-        b = Button(self.root, text="连接服务器")
+        b = Button(self.root, text="连接服务器", command=self.link_server)
         b.grid(row=0, column=3, sticky='e')
 
         l = Label(self.root, text=u"+++++++++++++"*10).grid(row=1, column=0, columnspan=10, sticky="w")
@@ -93,14 +156,20 @@ class UI:
 
         row = row + 1
         l = Label(self.root, text=u"+++++++++++++"*10).grid(row=row, column=0, columnspan=10, sticky="w")
+        row = row + 1
+        l = Label(self.root, text=u"服务端口").grid(row=row, column=0)
+        l = Entry(self.root, textvariable=self.server_port, width=8)
+        l.grid(row=row, column=1, sticky="w")
+        b = Button(self.root, text="启动服务器", command=self.start_server)
+        b.grid(row=row, column=2, sticky='e')
 
         row = row + 1
         Label(self.root, text=u"主站数据点:").grid(row=row, column=0, sticky="w")
         l = Label(self.root, text=u"主站地址:").grid(row=row, column=1)
-        l = Entry(self.root, textvariable=self.client_addr, width=5)
+        l = Entry(self.root, textvariable=self.server_addr, width=5)
         l.grid(row=row, column=2, sticky="w")
 
-        row = self.load_server_point("client.csv", row)
+        row = self.load_server_point("server.csv", row + 1)
 
         self.root.mainloop()
 
